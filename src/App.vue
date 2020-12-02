305fc5cc6c61956b34beb9f42a7fb195ac5bb53e
Tommy Pratama <tommyhpratama@gmail.com>
2020-12-02 10:45:49 +0000

    :octocat: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen w-screen inline-flex items-center justify-center">
+    <h1 class="text-3xl text-amber-500">Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      dark: "#121212",
+      white: "#fff",
+      bluegray: colors.blueGray,
+      coolgray: colors.coolGray,
+      gray: colors.trueGray,
+      truegray: colors.trueGray,
+      warmgray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightblue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
